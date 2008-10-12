6ed5f33256cce029deca97943330e461eee6ac0c
David Glick <davidglick@groundwire.org>
2008-10-12 16:49:50 +0000

    implemented type addition/removal
    svn path=/plone.app.dexterity/trunk/; revision=22867

--- plone/app/dexterity/browser/types/types.py
+++ plone/app/dexterity/browser/types/types.py
@@ -4,16 +4,35 @@ from zope.interface import Interface, implements
 from zope.component import getAllUtilitiesRegisteredFor, getUtility, getMultiAdapter, ComponentLookupError
 from zope.publisher.interfaces.browser import IBrowserPage
 from zope.publisher.browser import BrowserPage
+from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
 from zope import schema
 from zope.schema.interfaces import IField
 from z3c.form import field
 from plone.z3cform import layout
 from plone.z3cform.crud import crud
 from plone.dexterity.interfaces import IDexterityFTI
+from plone.dexterity.fti import DexterityFTI
 from zope.publisher.browser import BrowserPage
 from plone.app.dexterity.interfaces import ITypesEditingContext
+from Products.CMFCore.utils import getToolByName
+from plone.i18n.normalizer.interfaces import IURLNormalizer
 
-class RemoveOnlyForm(crud.EditForm):
+class IAddTypeSettings(Interface):
+    
+    title = schema.TextLine(
+        title = u'Type Name'
+        )
+
+    description = schema.Text(
+        title = u'Description',
+        required = False
+        )
+
+class TypeAddForm(crud.AddForm):
+    label = u'Add Content Type'
+    template = ViewPageTemplateFile('../form.pt')
+
+class TypeEditForm(crud.EditForm):
     
     label = None
     
@@ -24,18 +43,30 @@ class RemoveOnlyForm(crud.EditForm):
 class TypesListing(crud.CrudForm):
     
     view_schema = field.Fields(IItem).select('title')
-    addform_factory = crud.NullForm
-    editform_factory = RemoveOnlyForm
+    add_schema = IAddTypeSettings
+    
+    addform_factory = TypeAddForm
+    editform_factory = TypeEditForm
     
     def get_items(self):
         ftis = getAllUtilitiesRegisteredFor(IDexterityFTI)
         return [(fti.__name__, fti) for fti in ftis]
 
     def add(self, data):
-        return None
+        
+        id = getUtility(IURLNormalizer).normalize(data['title'])
+        # XXX validation
+
+        fti = DexterityFTI(id)
+        fti.id = id
+        fti.manage_changeProperties(**data)
+
+        ttool = getToolByName(self.context, 'portal_types')
+        ttool._setObject(id, fti)
 
     def remove(self, (id, item)):
-        return None
+        ttool = getToolByName(self.context, 'portal_types')
+        ttool.manage_delObjects([id])
 
     def link(self, item, field):
         if item.has_dynamic_schema:
