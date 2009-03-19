8d83cf6eb1bcbb079e339aa594a5d81dcd4d4fe2
Alec Mitchell <apm13@columbia.edu>
2009-03-19 19:27:17 +0000

    Use a factory-less behavior for related items.
    svn path=/plone.app.dexterity/trunk/; revision=26018

--- plone/app/dexterity/behaviors/related.py
+++ plone/app/dexterity/behaviors/related.py
@@ -1,5 +1,4 @@
-from zope.component import adapts
-from zope.interface import alsoProvides, implements
+from zope.interface import alsoProvides
 
 from z3c.relationfield.schema import RelationChoice, RelationList
 
@@ -10,9 +9,10 @@ from plone.directives import form
 class IRelatedItems(form.Schema):
     """Behavior interface to make a type support related items.
     """
-    
-    form.fieldset('categorization', label=u"Categorization", fields=['relatedItems'])
-    
+
+    form.fieldset('categorization', label=u"Categorization",
+                  fields=['relatedItems'])
+
     relatedItems = RelationList(
         title=u"Related Items",
         default=[],
@@ -22,20 +22,3 @@ class IRelatedItems(form.Schema):
         )
 
 alsoProvides(IRelatedItems, form.IFormFieldProvider)
-
-class RelatedItemsFactory(object):
-    """An adapter for storing related items directly on the object"""
-    adapts(IDexterityContent)
-    implements(IRelatedItems)
-    field_name = 'relatedItems'
-
-    def __init__(self, context):
-        self.context = context
-
-    # We can't use a FieldProperty because vocabulary validation won't work
-    # the vocabulary has content and we store RelationValues
-    def _get_related(self):
-        return getattr(self.context, self.field_name)
-    def _set_related(self, value):
-        setattr(self.context, self.field_name, value)
-    relatedItems = property(_get_related, _set_related)
