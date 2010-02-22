389f1270fc09abff3bc5db48c8f7453b7fec6e2f
David Glick <davidglick@groundwire.org>
2010-02-22 22:04:07 +0000

    make sure the plone.app.z3cform profile gets installed, for both new and upgraded installations
    svn path=/plone.app.dexterity/trunk/; revision=34311

---
+++ plone/app/dexterity/upgrades/to1.py
@@ -0,0 +1,6 @@
+from Products.CMFCore.utils import getToolByName
+
+def install_z3cform_profile(context):
+    gs = getToolByName(context, 'portal_setup')
+    profile = 'profile-plone.app.z3cform:default'
+    gs.runAllImportStepsFromProfile(profile, purge_old=False)
