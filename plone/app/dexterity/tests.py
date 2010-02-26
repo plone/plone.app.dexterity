2df554db4ca64c11763158a160a9b69106c8cae9
David Glick <davidglick@groundwire.org>
2010-02-26 07:54:11 +0000

    create a Dexterity test layer
    svn path=/plone.app.dexterity/trunk/; revision=34352

--- plone/app/dexterity/tests.py
+++ plone/app/dexterity/tests.py
@@ -4,17 +4,28 @@ from Testing import ZopeTestCase as ztc
 
 from Products.Five import zcml
 from Products.PloneTestCase import PloneTestCase as ptc
-from Products.PloneTestCase.layer import onsetup
+from Products.PloneTestCase.layer import PloneSite
 
 import plone.app.dexterity
 
-@onsetup
-def setup_product():
-    zcml.load_config('meta.zcml', plone.app.dexterity)
-    zcml.load_config('configure.zcml', plone.app.dexterity)
-
-setup_product()
 ptc.setupPloneSite(extension_profiles=['plone.app.dexterity:default'])
+
+class DexterityLayer(PloneSite):
+    
+    @classmethod
+    def setUp(cls):
+        zcml.load_config('meta.zcml', plone.app.dexterity)
+        zcml.load_config('configure.zcml', plone.app.dexterity)
+
+    @classmethod
+    def tearDown(cls):
+        pass
+
+class DexterityTestCase(ptc.PloneTestCase):
+    layer = DexterityLayer
+
+class DexterityFunctionalTestCase(ptc.FunctionalTestCase):
+    layer = DexterityLayer
 
 doc_tests = (
     'schema_events.txt',
@@ -30,11 +41,11 @@ def test_suite():
     return unittest.TestSuite(
         [ztc.FunctionalDocFileSuite(
             'tests/%s' % f, package='plone.app.dexterity',
-            test_class=ptc.FunctionalTestCase)
+            test_class=DexterityFunctionalTestCase)
             for f in functional_tests] + 
         [ztc.ZopeDocFileSuite(
             'tests/%s' % f, package='plone.app.dexterity',
-            test_class=ptc.FunctionalTestCase)
+            test_class=DexterityFunctionalTestCase)
             for f in doc_tests],
         )
 
