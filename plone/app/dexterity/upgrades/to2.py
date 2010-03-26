4f1f6648facff862806bc0a35477177490125f97
David Glick <davidglick@groundwire.org>
2010-03-26 16:25:01 +0000

    remove schemaeditor.css
    svn path=/plone.app.dexterity/trunk/; revision=35556

---
+++ plone/app/dexterity/upgrades/to2.py
@@ -0,0 +1,5 @@
+from Products.CMFCore.utils import getToolByName
+
+def remove_stylesheet(context):
+    cssreg = getToolByName(context, 'portal_css')
+    cssreg.unregisterResource('++resource++schemaeditor.css')
