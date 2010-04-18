53d6fb570ce885bc5729fd82c34a08a3df7632cc
David Glick <davidglick@groundwire.org>
2010-04-18 21:14:46 +0000

    make sure the overlays are styled on Plone 3
    svn path=/plone.app.dexterity/trunk/; revision=36185

--- plone/app/dexterity/upgrades/to3.py
+++ plone/app/dexterity/upgrades/to3.py
@@ -4,3 +4,8 @@ def install_jquerytools_profile(context):
     gs = getToolByName(context, 'portal_setup')
     profile = 'profile-plone.app.jquerytools:default'
     gs.runAllImportStepsFromProfile(profile, purge_old=False)
+    
+    # add overlay styles
+    profile = 'profile-plone.app.dexterity:default'
+    gs.runImportStepFromProfile(profile, 'cssregistry',
+                                run_dependencies=False, purge_old=False)
