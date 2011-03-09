1e1af20cf3a0709cd96ed4e5e7cc639500c418a4
Laurence Rowe <l@lrowe.co.uk>
2011-03-09 19:09:14 +0000

    Show exclude_from_nav only on add and edit forms
    svn path=/plone.app.dexterity/trunk/; revision=48006

--- plone/app/dexterity/behaviors/exclfromnav.py
+++ plone/app/dexterity/behaviors/exclfromnav.py
@@ -1,9 +1,7 @@
+from z3c.form.interfaces import IEditForm, IAddForm
 from zope.interface import alsoProvides
-
 from zope import interface, schema
-
 from plone.directives import form
-
 from plone.app.dexterity import MessageFactory as _
 
 class IExcludeFromNavigation(form.Schema):
@@ -18,5 +16,9 @@ class IExcludeFromNavigation(form.Schema):
                 description=_(u'help_exclude_from_nav', default=u'If selected, this item will not appear in the navigation tree'),
                 default=False
                 )
+
+    form.omitted('exclude_from_nav')
+    form.no_omit(IEditForm, 'exclude_from_nav')
+    form.no_omit(IAddForm, 'exclude_from_nav')
 
 alsoProvides(IExcludeFromNavigation, form.IFormFieldProvider)
