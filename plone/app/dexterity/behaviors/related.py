df3cd83c3957cef50569ef682e03cfa5c0d8a02b
David Glick <davidglick@groundwire.org>
2011-06-16 04:56:42 +0000

    register a separate behavior so it's a bit clearer that there's one that's deprecated and one that's not
    svn path=/plone.app.dexterity/branches/1.x/; revision=50480

--- plone/app/dexterity/behaviors/related.py
+++ plone/app/dexterity/behaviors/related.py
@@ -3,9 +3,3 @@ plone.app.relationfield.
 """
 from plone.app.relationfield.behavior import IRelatedItems
 IRelatedItems # pyflakes
-
-from zope.component import queryUtility
-from plone.behavior.interfaces import IBehavior
-
-def related_items_behavior_BBB():
-    return queryUtility(IBehavior, name=u'plone.app.relationfield.behavior.IRelatedItems')
