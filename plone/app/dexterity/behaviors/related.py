68da81ff42839209bc2589beb81e423850b4fc47
David Glick <davidglick@groundwire.org>
2011-07-02 19:18:55 +0000

    fix BBB alias
    svn path=/plone.app.dexterity/branches/1.x/; revision=50899

--- plone/app/dexterity/behaviors/related.py
+++ plone/app/dexterity/behaviors/related.py
@@ -1,5 +1,7 @@
 """BBB import for 'Related Items' behavior that was moved to
 plone.app.relationfield.
 """
-from plone.app.relationfield.behavior import IRelatedItems
-IRelatedItems # pyflakes
+from plone.app.relationfield.behavior import IRelatedItems as IBaseRelatedItems
+
+class IRelatedItems(IBaseRelatedItems):
+    pass
