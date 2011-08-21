e95d2d57e84168145315af44558c3a0c25a3cada
David Glick <davidglick@groundwire.org>
2011-08-21 23:12:48 +0000

    fix related items behavior
    svn path=/plone.app.dexterity/branches/1.x/; revision=51711

--- plone/app/dexterity/behaviors/related.py
+++ plone/app/dexterity/behaviors/related.py
@@ -3,5 +3,17 @@ plone.app.relationfield.
 """
 from plone.app.relationfield.behavior import IRelatedItems as IBaseRelatedItems
 
+from zope.interface import alsoProvides
+
+from plone.supermodel.interfaces import FIELDSETS_KEY
+from plone.supermodel.model import Fieldset
+
+from plone.autoform.interfaces import IFormFieldProvider
+
+from plone.app.dexterity import MessageFactory as _
+
+
 class IRelatedItems(IBaseRelatedItems):
     pass
+
+alsoProvides(IRelatedItems, IFormFieldProvider)
