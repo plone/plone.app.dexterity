b07f0f034a995dd2ecde7dbf24aee20d17397a1c
Jean Jordaan <jean.jordaan@gmail.com>
2011-10-18 15:02:25 +0700

    README.txt was renamed; fix reference in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='plone.app.dexterity',
       description="Dexterity is a content type framework for CMF applications, "
                   "with particular emphasis on Plone. It can be viewed as an "
                   "alternative to Archetypes that is more light-weight and modular.",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open("RELEASE_NOTES.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
