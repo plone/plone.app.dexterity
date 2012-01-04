4be345478305f6d7499e8327e39ef907fb6b310a
Giacomo Spettoli <giacomo.spettoli@gmail.com>
2012-01-04 20:27:20 +0100

    Added doc appendix: schema driven forms

--- docs/conf.py
+++ docs/conf.py
@@ -145,10 +145,10 @@ html_sidebars = {
 #html_additional_pages = {}
 
 # If false, no module index is generated.
-#html_use_modindex = True
+html_use_modindex = True
 
 # If false, no index is generated.
-#html_use_index = True
+html_use_index = True
 
 # If true, the index is split into individual pages for each letter.
 #html_split_index = False
