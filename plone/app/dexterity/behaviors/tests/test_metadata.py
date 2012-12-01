05eea26c02801d27107eb2ef21f63f5d46134180
Timo Stollenwerk <contact@timostollenwerk.net>
2012-12-01 21:37:38 +0100

    Whitespace

--- plone/app/dexterity/behaviors/tests/test_metadata.py
+++ plone/app/dexterity/behaviors/tests/test_metadata.py
@@ -4,28 +4,28 @@ import unittest
 
 
 class TestBasic(unittest.TestCase):
-    
+
     def _makeOne(self):
         class Dummy(object):
             pass
         dummy = Dummy()
         from plone.app.dexterity.behaviors.metadata import Basic
         return Basic(dummy)
-    
+
     def test_title_setter(self):
         b = self._makeOne()
         b.title = u'foo'
         self.assertEqual(u'foo', b.context.title)
-    
+
     def test_title_setter_rejects_bytestrings(self):
         b = self._makeOne()
         self.assertRaises(ValueError, setattr, b, 'title', 'føø')
-    
+
     def test_title_getter(self):
         b = self._makeOne()
         b.context.title = u'foo'
         self.assertEqual(u'foo', b.title)
-    
+
     def test_description_setter(self):
         b = self._makeOne()
         b.description = u'foo'
@@ -54,7 +54,7 @@ class TestCategorization(unittest.TestCase):
         b = self._makeOne()
         b.subjects = (u'føø',)
         self.assertEqual((u'føø',), b.context.subject)
-    
+
     def test_subjects_getter(self):
         b = self._makeOne()
         b.context.subject = (u'føø',)
