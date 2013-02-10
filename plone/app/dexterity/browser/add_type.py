2e7ca0f70be82f43320907d256f1ece94f5f5b9e
David Glick <david@glicksoftware.com>
2013-02-10 13:13:42 -0800

    redirect to fields after type is added

--- plone/app/dexterity/browser/add_type.py
+++ plone/app/dexterity/browser/add_type.py
@@ -12,6 +12,7 @@ class TypeAddForm(form.AddForm):
     label = _(u'Add Content Type')
     fields = field.Fields(ITypeSettings).select('title', 'id', 'description')
     id = 'add-type-form'
+    fti_id = None
 
     def create(self, data):
         id = data.pop('id')
@@ -22,9 +23,10 @@ class TypeAddForm(form.AddForm):
         if data['description']:
             data['description'] = data['description'].encode('utf8')
         data['i18n_domain'] = 'plone'
-        data['behaviors'] = "\n".join(['plone.app.dexterity.behaviors.metadata.IDublinCore',
-                                       'plone.app.content.interfaces.INameFromTitle',
-                                       ])
+        data['behaviors'] = "\n".join([
+            'plone.app.dexterity.behaviors.metadata.IDublinCore',
+            'plone.app.content.interfaces.INameFromTitle',
+        ])
         data['model_source'] = """
 <model xmlns="http://namespaces.plone.org/supermodel/schema">
     <schema>
@@ -41,9 +43,13 @@ class TypeAddForm(form.AddForm):
     def add(self, fti):
         ttool = getToolByName(self.context, 'portal_types')
         ttool._setObject(fti.id, fti)
+        self.fti_id = fti.id
         self.status = _(u"Type added successfully.")
 
     def nextURL(self):
-        return self.context.absolute_url()
+        url = self.context.absolute_url()
+        if self.fti_id is not None:
+            url += '/%s/@@fields' % self.fti_id
+        return url
 
 TypeAddFormPage = wrap_form(TypeAddForm)
