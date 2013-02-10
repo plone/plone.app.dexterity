f226193d74359f10ecf12e27e589fbd77cfe41ff
David Glick <david@glicksoftware.com>
2013-02-10 09:37:57 -0800

    remove id from overview page

--- plone/app/dexterity/browser/overview.py
+++ plone/app/dexterity/browser/overview.py
@@ -1,5 +1,4 @@
 from z3c.form import form, field
-from z3c.form.interfaces import DISPLAY_MODE
 
 from plone.app.dexterity.browser.layout import TypeFormLayout
 from plone.app.dexterity.interfaces import ITypeSettings
@@ -7,8 +6,7 @@ from plone.app.dexterity import MessageFactory as _
 
 
 class TypeOverviewForm(form.EditForm):
-    fields = field.Fields(ITypeSettings).select('id', 'title', 'description')
-    fields['id'].mode = DISPLAY_MODE
+    fields = field.Fields(ITypeSettings).select('title', 'description')
 
     def getContent(self):
         return self.context.fti
