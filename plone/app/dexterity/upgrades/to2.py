9fa7a2ee9538e572c80f5df29170d061bc08fa74
Timo Stollenwerk <contact@timostollenwerk.net>
2013-04-20 10:43:51 +0200

    Another Jenkins test + PEP8.

--- plone/app/dexterity/upgrades/to2.py
+++ plone/app/dexterity/upgrades/to2.py
@@ -1,5 +1,6 @@
 from Products.CMFCore.utils import getToolByName
 
+
 def remove_stylesheet(context):
     cssreg = getToolByName(context, 'portal_css')
     cssreg.unregisterResource('++resource++schemaeditor.css')
