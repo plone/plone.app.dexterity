abf4205b9ea944a0920e186e82bcf3aff26b3fc8
Eric Steele <EricSteele47@gmail.com>
2013-05-23 21:22:12 -0400

    Back to development: 2.0.9

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.0.8'
+version = '2.0.9.dev0'
 long_description = open("README.rst").read() + "\n" + \
     open("RELEASE_NOTES.txt").read() + "\n" + \
     open(os.path.join("docs", "HISTORY.txt")).read()
