bc6045d00958e16e6e183c18df191ae2113d7aaa
Gauthier Bastien <g.bastien@imio.be>
2013-06-11 10:51:25 +0200

    Make contents/containers views easy to override by defining parts in the template managed by overridable BrowserViews.  See #83

--- plone/app/dexterity/browser/render.py
+++ plone/app/dexterity/browser/render.py
@@ -6,3 +6,13 @@ from Products.Five import BrowserView
 # The widget rendering templates need to be Zope 3 templates
 class RenderWidgets(ViewMixinForTemplates, BrowserView):
     index = ViewPageTemplateFile('render_widgets.pt')
+
+
+# The widget rendering templates need to be Zope 3 templates
+class RenderGroups(ViewMixinForTemplates, BrowserView):
+    index = ViewPageTemplateFile('render_groups.pt')
+
+
+# The widget rendering templates need to be Zope 3 templates
+class RenderFolderListing(ViewMixinForTemplates, BrowserView):
+    index = ViewPageTemplateFile('render_folder_listing.pt')
