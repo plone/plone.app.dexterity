6e586f32c809ebbc3ec586712735da0f87b1463d
Steve McMahon <steve@dcn.org>
2013-11-07 16:30:32 -0800

    Add checks for bad imports; add import test

--- plone/app/dexterity/tests/test_import.py
+++ plone/app/dexterity/tests/test_import.py
@@ -3,14 +3,12 @@
 
 from DateTime.DateTime import DateTime
 from plone.app.dexterity.testing import DEXTERITY_INTEGRATION_TESTING
-from plone.app.dexterity.browser.importtypes import ZipFileImportContext
+from plone.app.dexterity.browser.importtypes import TypesZipFileImportContext
 from zope.component import getMultiAdapter
 from Products.CMFCore.utils import getToolByName
 
 import os.path
-import StringIO
 import unittest2 as unittest
-import zipfile
 
 
 class TestDexterityTypesImport(unittest.TestCase):
@@ -22,14 +20,26 @@ class TestDexterityTypesImport(unittest.TestCase):
         portal = self.layer['portal']
         types_tool = getToolByName(portal, 'portal_types')
 
-        zname = os.path.join(os.path.dirname(__file__), 'import', 'dexterity_export.zip')
+        zname = os.path.join(
+            os.path.dirname(__file__),
+            'import',
+            'dexterity_export.zip',
+        )
         f = open(zname, 'r')
-        icontext = ZipFileImportContext(types_tool, f)
+        icontext = TypesZipFileImportContext(types_tool, f)
 
         types_xml = icontext.readDataFile('types.xml')
-        self.assertTrue(types_xml, msg='Unable to read types.xml in sample import file')
+        self.assertTrue(
+            types_xml,
+            msg='Unable to read types.xml in sample import file',
+        )
 
-        self.assertTrue(isinstance(icontext.getLastModified('types.xml'), DateTime))
+        self.assertTrue(
+            isinstance(
+                icontext.getLastModified('types.xml'),
+                DateTime
+            )
+        )
 
         self.assertEqual(
             set(icontext.listDirectory('')),
@@ -41,8 +51,45 @@ class TestDexterityTypesImport(unittest.TestCase):
             set(['test_type_two.xml', 'test_type_one.xml'])
         )
 
+        # test importIsTypesOnly check
+        # self.assertTrue(icontext.importIsTypesOnly())
+
         f.close()
 
+    def testSampleImportStep(self):
+        """ Import our sample file
+        """
+
+        portal = self.layer['portal']
+        setup_tool = getToolByName(portal, 'portal_setup')
+        types_tool = getToolByName(portal, 'portal_types')
+        old_types = set(types_tool.listContentTypes())
+
+        handler = setup_tool.getImportStep(u'typeinfo')
+
+        zname = os.path.join(
+            os.path.dirname(__file__),
+            'import',
+            'dexterity_export.zip'
+        )
+        with open(zname, 'r') as f:
+            icontext = TypesZipFileImportContext(types_tool, f)
+            handler(icontext)
+
+        # Our types list should have our two new types
+        self.assertEqual(
+            set(types_tool.listContentTypes()) - old_types,
+            set(['test_type_one', 'test_type_two'])
+        )
+
+        # trying to create the context again should fail, since
+        # it would be importing existing types.
+        with open(zname, 'r') as f:
+            self.assertRaises(ValueError,
+                TypesZipFileImportContext,
+                types_tool,
+                f
+            )
 
 
 def test_suite():
