794364ca9f13e455fd3b794960a6cdc5993f91c2
Eric Steele <EricSteele47@gmail.com>
2014-03-01 23:50:28 -0500

    Preparing release 2.1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.0.12.dev0'
+version = '2.1.0'
 long_description = open("README.rst").read() + "\n" + \
     open("RELEASE_NOTES.txt").read() + "\n" + \
     open(os.path.join("docs", "HISTORY.txt")).read()
