376813b1915d3858b7af299181f5a62936c3d207
Timo Stollenwerk <tisto@plone.org>
2015-06-05 10:05:53 +0200

    Back to development: 2.1.7

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '2.1.6'
+version = '2.1.7.dev0'
 long_description = open("README.rst").read() + "\n" + \
     open("RELEASE_NOTES.rst").read() + "\n" + \
     open("CHANGES.rst").read()
