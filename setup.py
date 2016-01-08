09ccb444500a786cfb207d213e0ba7c17c92b626
Maurits van Rees <maurits@vanrees.org>
2016-01-08 20:18:52 +0100

    Preparing release 2.1.17
    [ci skip]

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '2.1.17.dev0'
+version = '2.1.17'
 long_description = open("README.rst").read() + "\n" + \
     open("RELEASE_NOTES.rst").read() + "\n" + \
     open("CHANGES.rst").read()
