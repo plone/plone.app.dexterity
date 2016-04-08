5edc2d27ac24dcc422fbd6a865a2492edfda6c2e
Jens W. Klein <jk@kleinundpartner.at>
2016-04-08 17:59:07 +0200

    Remove dups from TTW behavior FTI editor.

--- plone/app/dexterity/browser/behaviors.py
+++ plone/app/dexterity/browser/behaviors.py
@@ -1,17 +1,27 @@
 # -*- coding: utf-8 -*-
+from collections import Counter
+from operator import attrgetter
 from copy import deepcopy
 from plone.app.dexterity import _
 from plone.app.dexterity.browser.layout import TypeFormLayout
 from plone.app.dexterity.interfaces import ITypeSchemaContext
 from plone.behavior.interfaces import IBehavior
+from plone.behavior.registration import lookup_behavior_registration
+from plone.behavior.registration import BehaviorRegistrationNotFound
 from plone.dexterity.fti import DexterityFTIModificationDescription
-from z3c.form import field, form
+from z3c.form import field
+from z3c.form import form
 from z3c.form.browser.checkbox import SingleCheckBoxFieldWidget
 from zope import schema
 from zope.browserpage.viewpagetemplatefile import ViewPageTemplateFile
 from zope.component import adapter
 from zope.component import getUtilitiesFor
 from zope.lifecycleevent import modified
+
+TTW_BEHAVIOR_BLACKLIST = [
+    # skip deprecated behavior
+    'plone.app.dexterity.behaviors.related.IRelatedItems',
+]
 
 
 def behaviorConfigurationModified(object, event):
@@ -27,26 +37,42 @@ class BehaviorConfigurationAdapter(object):
         self.__dict__['fti'] = self.context.fti
 
     def __getattr__(self, name):
-        # return True if the behavior is present
-        # (sanity check: don't try unless the name has a period in it)
-        if '.' in name:
-            return name in self.fti.behaviors
-        else:
-            raise AttributeError
+        # be sure to get a valid value
+        reg = lookup_behavior_registration(name=name)
+        iid = reg.interface.__identifier__
+        return (
+            iid in self.fti.behaviors or
+            reg.name.encode('utf8') in self.fti.behaviors
+        )
 
     def __setattr__(self, name, value):
         # add or remove the behavior based on the value from the form
         behaviors = list(self.fti.behaviors)
-        if value and name not in behaviors:
-            behaviors.append(name)
-        elif not value and name in behaviors:
-            behaviors.remove(name)
+        reg = lookup_behavior_registration(name=name)
+        iid = reg.interface.__identifier__
+        if reg.name and iid in self.fti.behaviors:
+            behaviors.remove(iid)
+            bname = reg.name.encode('utf8')
+        else:
+            bname = iid
+        if value and bname not in behaviors:
+            behaviors.append(bname)
+        elif not value and bname in behaviors:
+            behaviors.remove(bname)
         self.fti.behaviors = behaviors
 
     def __iter__(self):
         # iterate through the present behaviors
-        for b in self.fti.behaviors:
-            yield b
+        for name in self.fti.behaviors:
+            try:
+                reg = lookup_behavior_registration(name=name)
+            except BehaviorRegistrationNotFound:
+                # ignore wrong names
+                continue
+            if reg.name:
+                yield reg.name.encode('utf8')
+            else:
+                yield name
 
 
 class TypeBehaviorsForm(form.EditForm):
@@ -64,25 +90,32 @@ class TypeBehaviorsForm(form.EditForm):
 
     @property
     def fields(self):
+        counts = Counter(
+            [reg.interface for name, reg in getUtilitiesFor(IBehavior)]
+        )
         fields = []
         for name, reg in getUtilitiesFor(IBehavior):
-            if name == 'plone.app.dexterity.behaviors.related.IRelatedItems':
-                # skip deprecated behavior
+            if name in TTW_BEHAVIOR_BLACKLIST:
+                # skip blacklisted
                 continue
-
-            f = schema.Bool(
-                __name__=str(name),
-                title=reg.title,
-                description=reg.description,
-                required=False
+            with_name = counts[reg.interface] == 2
+            if with_name and reg.name != name:
+                continue
+            fname = reg.name if reg.name else name
+            if isinstance(fname, unicode):
+                fname = fname.encode('utf8')
+            fields.append(
+                schema.Bool(
+                    __name__=fname,
+                    title=reg.title,
+                    description=reg.description,
+                    required=False
+                )
             )
-            fields.append(f)
-        fields = sorted(fields, key=lambda x: x.title)
-        fields = field.Fields(*fields)
-
-        for f in fields.values():
-            f.widgetFactory = SingleCheckBoxFieldWidget
-        return fields
+        form_fields = field.Fields(*sorted(fields, key=attrgetter('title')))
+        for ff in form_fields.values():
+            ff.widgetFactory = SingleCheckBoxFieldWidget
+        return form_fields
 
 
 class TypeBehaviorsPage(TypeFormLayout):
