04469e7b38d990fd6d0155c83778a6d301225021
esteele <eric@esteele.net>
2017-06-26 08:35:05 -0400

    Back to development: 2.4.4
    [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '2.4.3'
+version = '2.4.4.dev0'
 
 short_description = (
     'Dexterity is a content type framework for CMF  applications, '
