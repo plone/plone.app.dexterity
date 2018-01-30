4a090c2c5df864607046f5bba7486715467b1ebf
Maurits van Rees <maurits@vanrees.org>
2018-01-30 01:39:01 +0100

    Made license classifier same as license in setup.py
    [ci skip]

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
     classifiers=[
         'Framework :: Plone',
         'Framework :: Plone :: 5.1',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Topic :: Software Development :: Libraries :: Python Modules',
