f00421f8fec2aea34d77610486e755a69217669f
Philip Bauer <bauer@starzel.de>
2018-05-15 19:31:22 +0200

    fix UTF8Property in py3

--- plone/app/dexterity/browser/utils.py
+++ plone/app/dexterity/browser/utils.py
@@ -9,11 +9,11 @@ class UTF8Property(object):
 
     def __get__(self, inst, type=None):
         value = getattr(inst.context, self.name)
-        if isinstance(value, str):
+        if six.PY2 and isinstance(value, six.binary_type):
             value = value.decode('utf8')
         return value
 
     def __set__(self, inst, value):
-        if isinstance(value, six.text_type):
+        if six.PY2 and isinstance(value, six.text_type):
             value = value.encode('utf8')
         setattr(inst.context, self.name, value)
