9600a7ef31773030e6c538f4b4648d72675e234f
Philip Bauer <bauer@starzel.de>
2018-06-14 18:32:46 +0200

    fix most tests

--- plone/app/dexterity/tests/test_doctests.py
+++ plone/app/dexterity/tests/test_doctests.py
@@ -4,7 +4,8 @@ from plone.testing import layered
 
 import doctest
 import unittest
-
+import re
+import six
 
 tests = (
     'discussion.txt',
@@ -17,9 +18,30 @@ tests = (
 )
 
 
+class Py23DocChecker(doctest.OutputChecker):
+    def check_output(self, want, got, optionflags):
+        if six.PY2:
+            got = re.sub('zExceptions.NotFound', 'NotFound', got)
+            got = re.sub('zope.interface.interfaces.ComponentLookupError', 'ComponentLookupError', got)
+            got = re.sub('zope.testbrowser.browser.LinkNotFoundError', 'LinkNotFoundError', got)
+            got = re.sub("u'(.*?)'", "'\\1'", want)
+            want = re.sub("b'(.*?)'", "'\\1'", want)
+        return doctest.OutputChecker.check_output(self, want, got, optionflags)
+
+
 def test_suite():
-    return unittest.TestSuite(
-        [layered(doctest.DocFileSuite(f, optionflags=doctest.ELLIPSIS),
-                 layer=DEXTERITY_FUNCTIONAL_TESTING)
-            for f in tests]
-    )
+    suite = unittest.TestSuite()
+    OPTIONFLAGS = (doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
+    for testfile in tests:
+        suite.addTest(
+            layered(
+                doctest.DocFileSuite(
+                    testfile,
+                    optionflags=OPTIONFLAGS,
+                    # package='plone.app.dexterity.tests',
+                    checker=Py23DocChecker(),
+                ),
+                layer=DEXTERITY_FUNCTIONAL_TESTING
+            )
+        )
+    return suite
