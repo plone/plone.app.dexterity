b98e70de40166a516f9e2c7b857f16fd16a78e11
Timo Stollenwerk <stollenwerk@kitconcept.com>
2018-06-24 22:47:21 +0200

    Preparing release 2.5.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '2.4.11.dev0'
+version = '2.5.0'
 
 short_description = (
     'Dexterity is a content type framework for CMF  applications, '
