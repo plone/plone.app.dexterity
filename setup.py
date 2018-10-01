8c0092f5784cf39ada6e53a07be7fce1ffa632fa
Maurits van Rees <maurits@vanrees.org>
2018-10-01 17:55:00 +0200

    Removed unneeded exclude ez_setup.py from setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     author_email='dexterity-development@googlegroups.com',
     url='http://plone.org/products/dexterity',
     license='GPL',
-    packages=find_packages(exclude=['ez_setup']),
+    packages=find_packages(),
     namespace_packages=['plone', 'plone.app'],
     include_package_data=True,
     zip_safe=False,
