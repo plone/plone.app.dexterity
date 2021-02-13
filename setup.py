e816072e6748be5a44ad495f365f04eefbdd38ab
Peter Mathis <petschki@users.noreply.github.com>
2021-02-13 09:56:43 +0100

    Barceloneta LTS (#321)
    * update markup for bootstrap
    
    * use icon from icon resolver
    
    * use explicit icon name
    
    * fix test
    
    * major version bump
    
    Co-authored-by: Peter Holzer <peter.holzer@agitator.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '2.6.10.dev0'
+version = '3.0.0.dev0'
 
 short_description = (
     'Dexterity is a content type framework for CMF  applications, '
