9e09899e48fdeefe6c63dc69573b8dbae81e3c3b
Philipp Auersperg-Castell <phil@bluedynamics.com>
2022-03-25 11:58:54 +0100

    textindexer works WIP

--- plone/app/dexterity/textindexer/__init__.py
+++ plone/app/dexterity/textindexer/__init__.py
@@ -2,8 +2,12 @@
 """Dynamic SearchableText index for dexterity content types
 """
 
-from collective.dexteritytextindexer import utils
+from plone.app.dexterity.textindexer import utils
 from plone.app.dexterity.behaviors.metadata import IBasic
+
+from plone.app.dexterity.textindexer.interfaces import IDynamicTextIndexExtender, IDexterityTextIndexFieldConverter
+
+from plone.app.dexterity.textindexer.directives import searchable, SEARCHABLE_KEY
 
 utils.searchable(IBasic, 'title')
 utils.searchable(IBasic, 'description')
