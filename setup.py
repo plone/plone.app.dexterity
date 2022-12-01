644007ac3c827c44f659c334e19fa10b56c4748c
Maurits van Rees <maurits@vanrees.org>
2022-12-02 00:29:42 +0100

    Require at least Python 3.8.
    [ci skip]

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
     namespace_packages=["plone", "plone.app"],
     include_package_data=True,
     zip_safe=False,
+    python_requires=">=3.8",
     install_requires=[
         # Dexterity
         "plone.app.textfield",
