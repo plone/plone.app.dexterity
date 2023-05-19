bb8f2c8a892f00364006e8ffef95f98a417ab981
Jens W. Klein <jk@kleinundpartner.at>
2023-05-19 16:58:01 +0200

    move plone.allowdiscussion behavior to plone.app.discussion

--- plone/app/dexterity/behaviors/discussion.py
+++ plone/app/dexterity/behaviors/discussion.py
@@ -1,39 +1,6 @@
-from plone.app.dexterity import _
-from plone.autoform import directives
-from plone.autoform.interfaces import IFormFieldProvider
-from plone.supermodel import model
-from z3c.form.interfaces import IAddForm
-from z3c.form.interfaces import IEditForm
-from zope import schema
-from zope.interface import provider
-from zope.schema.vocabulary import SimpleTerm
-from zope.schema.vocabulary import SimpleVocabulary
+from zope.deferredimport import deprecated
 
-
-options = SimpleVocabulary(
-    [
-        SimpleTerm(value=True, title=_("Yes")),
-        SimpleTerm(value=False, title=_("No")),
-    ]
+deprecated(
+    "IAllowDiscussion is here deprecated. Import from plone.app.z3cform.widgets.select instead (will be removed in Plone 7)"
+    IAllowDiscussion="plone.app.discussion.behaviors:IAllowDiscussion",
 )
-
-
-@provider(IFormFieldProvider)
-class IAllowDiscussion(model.Schema):
-    model.fieldset(
-        "settings",
-        label=_("Settings"),
-        fields=["allow_discussion"],
-    )
-
-    allow_discussion = schema.Choice(
-        title=_("Allow discussion"),
-        description=_("Allow discussion for this content object."),
-        vocabulary=options,
-        required=False,
-        default=None,
-    )
-
-    directives.omitted("allow_discussion")
-    directives.no_omit(IEditForm, "allow_discussion")
-    directives.no_omit(IAddForm, "allow_discussion")
