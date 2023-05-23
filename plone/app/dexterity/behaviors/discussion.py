3778d627d8ea5b75843a0db06cff168a51b574a7
Jens W. Klein <jk@kleinundpartner.at>
2023-05-23 11:39:30 +0200

    Update plone/app/dexterity/behaviors/discussion.py
    Co-authored-by: David Glick <david@glicksoftware.com>

--- plone/app/dexterity/behaviors/discussion.py
+++ plone/app/dexterity/behaviors/discussion.py
@@ -1,6 +1,6 @@
 from zope.deferredimport import deprecated
 
 deprecated(
-    "IAllowDiscussion is here deprecated. Import from plone.app.z3cform.widgets.select instead (will be removed in Plone 7)"
+    "IAllowDiscussion is deprecated here. Import from plone.app.discussion.behaviors instead (will be removed in Plone 7)"
     IAllowDiscussion="plone.app.discussion.behaviors:IAllowDiscussion",
 )
