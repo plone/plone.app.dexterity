c5d845819c9c3379ea9b18aa693f80db021fcac5
Jens W. Klein <jk@kleinundpartner.at>
2024-05-15 16:03:42 +0200

    better phraseing and changelog, - SyntaxError

--- plone/app/dexterity/behaviors/discussion.py
+++ plone/app/dexterity/behaviors/discussion.py
@@ -1,6 +1,7 @@
 from zope.deferredimport import deprecated
 
+
 deprecated(
-    "IAllowDiscussion is deprecated here. Import from plone.app.discussion.behaviors instead (will be removed in Plone 7)"
+    "IAllowDiscussion import from here is deprecated. Import from plone.app.discussion.behaviors instead (will be removed in Plone 7)",
     IAllowDiscussion="plone.app.discussion.behaviors:IAllowDiscussion",
 )
