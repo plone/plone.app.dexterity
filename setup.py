287d811275ed5ebc29cdde4a68ae6dae09bcbdf2
Maurits van Rees <maurits@vanrees.org>
2025-03-26 10:00:24 +0100

    Back to development: 3.2.3
    [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = "3.2.2"
+version = "3.2.3.dev0"
 
 short_description = (
     "Dexterity is a content type framework for CMF  applications, "
