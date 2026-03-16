9d3f3a1ea81cb1484a1a66d1c182c53fc2a27978
Maurits van Rees <maurits@vanrees.org>
2026-03-16 16:54:52 +0100

    Back to development: 4.1.3
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from setuptools import find_packages
 from setuptools import setup
 
-version = "4.1.2"
+version = "4.1.3.dev0"
 
 short_description = (
     "Dexterity is a content type framework for CMF  applications, "
