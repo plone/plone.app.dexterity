b0fb9233d8edba9dca7cdbd0651807dde3f21e12
Franco Pellegrini <frapell@gmail.com>
2026-05-30 12:27:12 +0200

    Move plone.app.dexterity views to plone.app.layout. refs plone/Products.CMFPlone#3953

--- src/plone/app/dexterity/browser/fields.py
+++ src/plone/app/dexterity/browser/fields.py
@@ -1,40 +1,11 @@
-from importlib.metadata import distribution
-from importlib.metadata import PackageNotFoundError
-from plone.app.dexterity import _
-from plone.app.dexterity.browser.layout import TypeFormLayout
-from plone.schemaeditor.browser.schema.listing import ReadOnlySchemaListing
-from plone.schemaeditor.browser.schema.listing import SchemaListing
-from z3c.form import button
-
-try:
-    distribution("plone.resourceeditor")
-    HAS_RESOURCEEDITOR = True
-except PackageNotFoundError:
-    HAS_RESOURCEEDITOR = False
-
-# We want to add a Plone-specific feature to the SchemaListing
-# form from plone.schemaeditor. We'll do this by subclassing, then
-# adding the plone-specific button for the ace model editor.
+import zope.deferredimport
 
 
-class EnhancedSchemaListing(SchemaListing):
-    def handleModelEdit(self, action):
-        self.request.response.redirect("@@modeleditor")
+zope.deferredimport.initialize()
 
-
-if HAS_RESOURCEEDITOR:
-    but = button.Button("modeleditor", title="Edit XML Field Model")
-    EnhancedSchemaListing.buttons += button.Buttons(but)
-    handler = button.Handler(but, EnhancedSchemaListing.handleModelEdit)
-    EnhancedSchemaListing.handlers.addHandler(but, handler)
-
-
-class TypeFieldsPage(TypeFormLayout):
-    label = _("Fields")
-
-    @property
-    def form(self):
-        if self.context.fti.hasDynamicSchema:
-            return EnhancedSchemaListing
-        else:
-            return ReadOnlySchemaListing
+zope.deferredimport.deprecated(
+    "Please use from plone.app.layout.dexterity.fields import "
+    "EnhancedSchemaListing, TypeFieldsPage instead.",
+    EnhancedSchemaListing="plone.app.layout.dexterity.fields:EnhancedSchemaListing",
+    TypeFieldsPage="plone.app.layout.dexterity.fields:TypeFieldsPage",
+)
--- src/plone/app/dexterity/browser/overview.py
+++ src/plone/app/dexterity/browser/overview.py
@@ -1,34 +1,11 @@
-from plone.app.dexterity import _
-from plone.app.dexterity.browser.layout import TypeFormLayout
-from plone.app.dexterity.interfaces import ITypeSettings
-from Products.CMFCore.interfaces import IFolderish
-from z3c.form import field
-from z3c.form import form
-from zope.browserpage.viewpagetemplatefile import ViewPageTemplateFile
-from zope.dottedname.resolve import resolve as resolveDottedName
+import zope.deferredimport
 
 
-class TypeOverviewForm(form.EditForm):
-    enableCSRFProtection = True
-    template = ViewPageTemplateFile("overview.pt")
+zope.deferredimport.initialize()
 
-    @property
-    def fields(self):
-        # if this type's class is not a container,
-        # remove the field for filtering contained content types
-        klass = resolveDottedName(self.context.fti.klass)
-        fields = field.Fields(ITypeSettings)
-        filtered = fields.select(
-            "title", "description", "allowed_content_types", "filter_content_types"
-        )
-        if not IFolderish.implementedBy(klass):
-            del filtered["filter_content_types"]
-        return filtered
-
-    def getContent(self):
-        return self.context.fti
-
-
-class TypeOverviewPage(TypeFormLayout):
-    form = TypeOverviewForm
-    label = _("Overview")
+zope.deferredimport.deprecated(
+    "Please use from plone.app.layout.dexterity.overview import "
+    "TypeOverviewForm, TypeOverviewPage instead.",
+    TypeOverviewForm="plone.app.layout.dexterity.overview:TypeOverviewForm",
+    TypeOverviewPage="plone.app.layout.dexterity.overview:TypeOverviewPage",
+)
