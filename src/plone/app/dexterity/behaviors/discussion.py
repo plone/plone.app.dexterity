713b793a6df347e72084f9700b0f0c8142895d74
David Glick <david@glicksoftware.com>
2026-08-21 12:00:20 -0700

    Fix deferred import of IAllowDiscussion

--- src/plone/app/dexterity/behaviors/discussion.py
+++ src/plone/app/dexterity/behaviors/discussion.py
@@ -1,6 +1,6 @@
 from zope.deferredimport import deprecated
 
 deprecated(
-    "IAllowDiscussion import from here is deprecated. Import from plone.app.discussion.behaviors instead (will be removed in Plone 7)",
-    IAllowDiscussion="plone.app.discussion.behaviors:IAllowDiscussion",
+    "IAllowDiscussion import from here is deprecated. Import from plone.app.discussion.behavior instead (will be removed in Plone 7)",
+    IAllowDiscussion="plone.app.discussion.behavior:IAllowDiscussion",
 )
